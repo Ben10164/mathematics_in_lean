c78db3ff16f5cfed4a0395214202c2052cb9e8af
Ben Puryear <bpuryear@microsoft.com>
2024-11-20 08:31:54 -0800

    overview

---
+++ my_files/C01_S02_Overview.lean
@@ -0,0 +1,68 @@
+import MIL.Common
+open Nat
+
+-- These are pieces of data.
+#check 2 + 2
+#check add_assoc
+
+def f (x : ℕ) :=
+  x + 3
+
+#check f
+
+#eval f 2
+#eval f (f 2)
+#eval f (f (f 2))
+
+def g (x : ℕ) (f : ℕ →  ℕ) :=
+  x + f x
+
+#eval g 5 f -- 5 + (5 + 3)
+#eval g 5 (g 5 f) -- 5 + (5 + (5 + 3))
+#eval 5 + (5 + (5 + 3))
+
+-- These are propositions, of type `Prop`.
+#check 2 + 2 = 4
+
+def FermatLastTheorem :=
+  ∀ x y z n : ℕ, n > 2 ∧ x * y * z ≠ 0 → x ^ n + y ^ n ≠ z ^ n
+
+#check FermatLastTheorem
+
+-- These are proofs of propositions.
+theorem easy : 2 + 2 = 4 :=
+  rfl
+
+#check easy
+
+theorem hard : FermatLastTheorem :=
+  sorry
+
+#check hard
+
+-- Here are some proofs.
+example : ∀ m n : Nat, Even n → Even (m * n) := fun m n ⟨k, (hk : n = k + k)⟩ ↦
+  have hmn : m * n = m * k + m * k := by rw [hk, mul_add]
+  show ∃ l, m * n = l + l from ⟨_, hmn⟩
+
+example : ∀ m n : Nat, Even n → Even (m * n) :=
+fun m n ⟨k, hk⟩ ↦ ⟨m * k, by rw [hk, mul_add]⟩
+
+example : ∀ m n : Nat, Even n → Even (m * n) := by
+  -- Say `m` and `n` are natural numbers, and assume `n = 2 * k`.
+  rintro m n ⟨k, hk⟩ -- introduces k, and hk (which is k + k)
+  -- We need to prove `m * n` is twice a natural number. Let's show it's twice `m * k`.
+  use m * k
+  -- Substitute for `n`,
+  rw [hk]
+  -- and now it's obvious.
+  ring
+
+example : ∀ m n : Nat, Even n → Even (m * n) := by
+  rintro m n ⟨k, hk⟩
+  use m * k
+  rw [hk]
+  ring
+
+example : ∀ m n : Nat, Even n → Even (m * n) := by
+  intros; simp [*, parity_simps]
