740ec2af0f41dbc6445e4076409769e047f14c34
Ben Puryear <bpuryear@microsoft.com>
2025-07-02 10:58:01 -0700

    bump to lean4:v4.20.0

--- my_files/C09_Linear_Algebra/S03_Endomorphisms.lean
+++ my_files/C09_Linear_Algebra/S03_Endomorphisms.lean
@@ -27,28 +27,64 @@ example (φ : End K V) : aeval φ (X : K[X]) = φ :=
 
 
 #check Submodule.eq_bot_iff
+-- p = ⊥ ↔ ∀ x ∈ p, x = 0
 #check Submodule.mem_inf
+-- x ∈ p ⊓ q ↔ x ∈ p ∧ x ∈ q
 #check LinearMap.mem_ker
+-- y ∈ ker f ↔ f y = 0
 
 example (P Q : K[X]) (h : IsCoprime P Q) (φ : End K V) :
     ker (aeval φ P) ⊓ ker (aeval φ Q) = ⊥ := by
   rw [Submodule.eq_bot_iff]
+  -- ⊢ ∀ x ∈ ker ((aeval φ) P) ⊓ ker ((aeval φ) Q), x = 0
   rintro x
   rw [Submodule.mem_inf]
-  rw [mem_ker]
-  rw [mem_ker]
+  -- ⊢ x ∈ ker ((aeval φ) P) ∧ x ∈ ker ((aeval φ) Q) → x = 0
+  rw [mem_ker, mem_ker]
+  -- ⊢ ((aeval φ) P) x = 0 ∧ ((aeval φ) Q) x = 0 → x = 0
 
   rintro ⟨hx, hy⟩
   rcases h with ⟨A, B, hAB⟩
+  -- IsCoprime P Q ⇒ A B : K[X], hAB : A * P + B * Q = 1
 
-  rcases A with ⟨p,q, ⟨ rr,ppp⟩⟩
+  -- congr: Congruency
+  -- h : a = b then congr(1 + $h) : 1 + a = 1 + b
+  -- we have A * P + B * Q = 1, and we want to make an aeval φ _ x for both sides
+  -- then we can expand and simplify values to 0
+  have := congr(((aeval φ) $hAB.symm) x)
+  -- ((aeval φ) 1) x = ((aeval φ) (A * P + B * Q)) x
 
-  -- have  :=
-  --   congr((aeval φ) $hAB.symm x)
-  
+  -- lets simplify *this* to be only x on the left side
+  -- this : ((aeval φ) 1) x = ((aeval φ) (A * P + B * Q)) x
+  rw [map_one] at this
+  -- this : 1 x = ((aeval φ) (A * P + B * Q)) x
+  rw [End.one_apply] at this
+  -- this : x = ((aeval φ) (A * P + B * Q)) x
 
+  -- now we can rewrite x
+  rw [this]
+  -- ⊢ ((aeval φ) (A * P + B * Q)) x = 0
 
-  sorry
+  -- now for a whole lot of expanding
+  rw [map_add]
+  -- ⊢ ((aeval φ) (A * P) + (aeval φ) (B * Q)) x = 0
+  rw [map_mul, map_mul]
+  -- ⊢ ((aeval φ) A * (aeval φ) P + (aeval φ) B * (aeval φ) Q) x = 0
+  rw [add_apply]
+  -- ⊢ ((aeval φ) A * (aeval φ) P) x + ((aeval φ) B * (aeval φ) Q) x = 0
+  rw [End.mul_apply, End.mul_apply]
+  -- ⊢ ((aeval φ) A) (((aeval φ) P) x) + ((aeval φ) B) (((aeval φ) Q) x) = 0
+
+  -- now that we are totally expanded, we can use hx and hy to simplify down to 0 for some
+  rw [hx]
+  rw [hy]
+
+  -- since it is aeval _ _ 0, that is always 0, so we can reduce there
+  rw [map_zero, map_zero]
+
+  -- 0 + 0 = 0 is trivial
+  rw [add_eq_left]
+
 
 #check Submodule.add_mem_sup
 #check map_mul
